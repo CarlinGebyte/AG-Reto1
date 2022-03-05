f2d584cf7c1846f9882581ceae06d7dfd51d24a4
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-04 23:22:21 -0500

    completed

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ function closeNav() {
 function facBorderBottom() {
   document.getElementById('flush-headingFour').classList.contains('border-bottom') ? document.getElementById('flush-headingFour').classList.remove('border-bottom') : document.getElementById('flush-headingFour').classList.add('border-bottom')
 }
+
+const form = document.getElementById('form');
+const mail = document.getElementById('mail-to');
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  alert('hola')
+  mail.setAttribute('href', `mailto:correo@gmail.com?subject=${data.mail}&body=I need you to add a new browser`);
+  mail.click();
+
+})
